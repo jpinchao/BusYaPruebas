4a1ce9658597f71a92823b1e142d8a191b4900e5
Luis Felipe Vélez Garcia <luvelezg@unal.edu.co>
2023-10-29 16:08:39 -0500

    Se actualizaron seeders

--- database/seeders/TiposEmpleadosSeeder.php
+++ database/seeders/TiposEmpleadosSeeder.php
@@ -15,12 +15,12 @@ class TiposEmpleadosSeeder extends Seeder
     {
         $tipo_empleado= new Tipo_empleado();
         $tipo_empleado->nombre = "Administrador TI";
-        $tipo_empleado->descripcion = "Capacidad de interaccion administrativa con las herramientas tecnologicas de la empresa";
+        $tipo_empleado->descripcion = "Persona con permisos administrativos y tecnologicos de la empresa";
         $tipo_empleado->save();
 
         $tipo_empleado= new Tipo_empleado();
         $tipo_empleado->nombre = "Conductor-Urbano";
-        $tipo_empleado->descripcion = "Persona que cpnduce los vehiculos en el area Urbana";
+        $tipo_empleado->descripcion = "Persona que conduce los vehiculos en el area Urbana";
         $tipo_empleado->save();
 
     }
